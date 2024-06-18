fbd7f15f49904637a25be8301f4f1129ef5cffcc
Saihtam77 <geroffroym777@gmail.com>
2024-06-18 19:31:47 +0200

    test

--- src/app/Services/commentaires.service.ts
+++ src/app/Services/commentaires.service.ts
@@ -9,7 +9,7 @@ import { Commentaire } from '../../../Data_types/Projets_types';
 export class CommentairesService {
   private commentairesSubject = new BehaviorSubject<Commentaire[]>([]);
   commentaires$ = this.commentairesSubject.asObservable();
-  url = "http://localhost:5147";
+  url = "http://localhost:5147"; 
   
   constructor(private http: HttpClient) { }
 
@@ -23,11 +23,6 @@ export class CommentairesService {
 
   getCommentaireById(id: number) {
     return this.http.get<Commentaire>(`${this.url}/commentaires/${id}`);
-  }
-
-  getCommentaireByTacheId(Tacheid: number) {
-    this.commentaires$.subscribe(commentaires => commentaires.filter(commentaire => commentaire.tacheId === Tacheid));
-    return this.commentaires$;
   }
 
   deleteCommentaire(id: number) {
